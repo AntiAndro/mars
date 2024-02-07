77e5dee3dc059d6792a57ba8baf67f37543a0df6
AntiAndro <151755086+AntiAndro@users.noreply.github.com>
2024-02-07 19:40:46 +0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,40 @@
+from flask import Flask, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    prom = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+            'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(prom)
+
+
+@app.route('/image_mars')
+def image():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                  </body>
+                </html>
+    <img src="{url_for('static', filename='img/mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
